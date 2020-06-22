22d2129328c563fdf4931e78c2b1d28224bf1537
y24er <1028704486@qq.com>
2020-06-22 13:51:37 +0800

    commit

---
+++ multiplication_table/index.js
@@ -0,0 +1,6 @@
+for (let i = 1; i <= 9; i++) {
+	for (let j = 1; j <= i; j++) {
+		document.write(i + "*" + j + "=" + i * j + "\t");
+	}
+	document.write("<br/>");
+}
---
+++ number/index.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 20; i++) {
+	if (i == 0)
+		continue;
+	if (i % 2 == 0) {
+		console.log(i + " is even number");
+	} else {
+		console.log(i + " is odd number");
+	}
+}
